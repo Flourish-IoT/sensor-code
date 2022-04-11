0c2e7b0ae50e81712a854a6c3f050ef2800b89b6
dmjman <dmb484@drexel.edu>
2022-04-11 18:41:24 -0400

    Sensor code now verifying for Nano 33 IOT

--- dht11-seesaw-arduino/dht11-seesaw-arduino-code/dht11-seesaw-arduino-code.ino
+++ dht11-seesaw-arduino/dht11-seesaw-arduino-code/dht11-seesaw-arduino-code.ino
@@ -1,14 +1,5 @@
-/*
-	Author      : David Bicer, Jr. -- dmjman
-	File        : dht11-seesaw-arduino-code.ino
-	Project     : Flourish
-	Created On  : 2021-11-10
-	Description : Program to read temperature, capacitance, and humidity from both the Inland DHT11 sensor and 
-	              Adafruit Seesaw I2C Soil Moisture Sensor.
-*/
-
-#include <dht.h>                                                                                                        // Library for the DHT
-#include "adafruit-seesaw.h"
+#include "Adafruit_seesaw.h"
+#include "DHT.h"
 
 uint8_t  const ANALOG_PIN_1          = A1;
 uint8_t  const ANALOG_PIN_2          = A2;
@@ -23,8 +14,9 @@ uint8_t  const SEESAW_I2C_ADDRESS    = 0x36;
 uint16_t const SERIAL_BAUD_RATE      = 9600;                                                                            // 9600 bps should be plenty for our purposes
 char     const DEGREE_SYMBOL         = 248;
 
-dht            * humidityTemperatureSensor = new dht();                                                                 // Pointers use less memory than declared -- maybe able to write our own library
-Seesaw::Seesaw * seesawSensor              = new Seesaw::Seesaw();
+DHT             * humidityTemperatureSensor = new DHT(DIGITAL_PIN_7, DHT11);
+Adafruit_seesaw * seesawSensor              = new Adafruit_seesaw();
+
 
 struct DHT11Results 
 {
@@ -32,23 +24,28 @@ struct DHT11Results
 	uint8_t temperature;                                                                                            // Temperature property is double, but as accurate as uint8_t
 };
 
+struct SeesawResults
+{
+	float temperature;
+	uint16_t capacitance;
+};
+
 DHT11Results * readDHT11Sensor() 
 {
-	DHT11Results * const dht11Results                    = (DHT11Results *) malloc(sizeof(DHT11Results));           // Allocate address for results struct
-	uint8_t        const humidityTemperatureSensorStatus = humidityTemperatureSensor->read11(DIGITAL_PIN_7);        // Our device is DHT 11, so use read11 method
+	DHT11Results * const dht11Results = (DHT11Results *) malloc(sizeof(DHT11Results));                               // Allocate address for results struct
+  
+	dht11Results->humidity    = humidityTemperatureSensor->readHumidity();
+	dht11Results->temperature = humidityTemperatureSensor->readTemperature();
 	
-	dht11Results->humidity    = humidityTemperatureSensor->humidity;                                                // Humidity and temperature stored in DHT object
-	dht11Results->temperature = humidityTemperatureSensor->temperature; 
-
 	return dht11Results;
 } 
 
-Seesaw::SeesawResults * readSeesawSensor() 
+SeesawResults * readSeesawSensor() 
 {
-	Seesaw::SeesawResults * const seesawResults = (Seesaw::SeesawResults *) malloc(sizeof(Seesaw::SeesawResults));
+	SeesawResults * const seesawResults = (SeesawResults *) malloc(sizeof(SeesawResults));
 	
-	seesawResults->temperature = seesawSensor->getTemperature();
-	seesawResults->capacitance = seesawSensor->getCapacitance();
+	seesawResults->temperature = seesawSensor->getTemp();
+	seesawResults->capacitance = seesawSensor->touchRead(0);
 	
 	return seesawResults;
 }
@@ -61,12 +58,13 @@ void setup()                                                                    
 		for (;;) 
 			delay(5000);
 	}
+	humidityTemperatureSensor->begin();
 }
 
 void loop()                                                                                                            // Runs continuously until device is manually stopped
 {
-	DHT11Results          * const dht11Results  = readDHT11Sensor();
-	Seesaw::SeesawResults * const seesawResults = readSeesawSensor();
+	DHT11Results  * const dht11Results  = readDHT11Sensor();
+	SeesawResults * const seesawResults = readSeesawSensor();
 
 	Serial.println("Temperature: " + String(dht11Results->temperature) + DEGREE_SYMBOL + "C");
 	Serial.println("Humidity: " + String(dht11Results->humidity) + "%");
