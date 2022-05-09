ff0a7ee52b6acb974fdcbc7baf18af9495a3467d
dmjman <dmb484@drexel.edu>
2022-05-09 16:51:27 -0400

    Split out all sensor code

--- src/sensors/sensor_operations.h
+++ src/sensors/sensor_operations.h
@@ -1,9 +1,38 @@
-#include "./sensor_results.h"
+#include "Adafruit_seesaw.h"
+#include "Adafruit_VEML7700.h"
+#include "DHT.h"
 
-namespace SensorOperations
+#include "./sensor_results.h"
+#include "../common.h"
+
+class SensorOperations
 {
-	SensorResults::DHT11Results  * readDHT11Sensor();
-	SensorResults::SeesawResults * readSeesawSensor();
-	SensorResults::LuxResults    * readLuxSensor();
-	void                           setupSensors();
-}
+public:
+	SensorOperations()
+	{
+		this->_humidityTemperatureSensor = new DHT(DIGITAL_PIN_7, DHT11);
+		this->_seesawSensor              = new Adafruit_seesaw();
+		this->_luxSensor                 = new Adafruit_VEML7700();
+	};
+
+	inline void onError() const
+	{
+		digitalWrite(RED_LED, HIGH);
+	}
+
+	inline void clearError() const
+	{
+		digitalWrite(RED_LED, LOW);
+	}
+
+	void setupSensors() const;
+	void readSensors()  const;
+private:
+	DHT               * _humidityTemperatureSensor;
+	Adafruit_seesaw   * _seesawSensor;
+	Adafruit_VEML7700 * _luxSensor;
+
+	SensorResults::DHT11Results  * readDHT11Sensor()  const;
+	SensorResults::SeesawResults * readSeesawSensor() const;
+	SensorResults::LuxResults    * readLuxSensor()    const;
+};
